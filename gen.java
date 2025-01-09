0b646576131093fcede02b058c642a39142891f2
Abhineshwar Jaiswar <131228879+OttoRadiant@users.noreply.github.com>
2025-01-09 10:58:22 +0530

    Update generics.java

--- gen.java
+++ gen.java
@@ -1,7 +1,29 @@
 
-// Aim : Write a Java Program to demonstrate a Generic Class.
+//: Write a Java Program to demonstrate a Generic Class.
+class Test<T> {
+T obj;
+Test(T obj) { this.obj = obj; } // constructor
+public T getObject() { return this.obj; }
+}
+class Main {
+public static void main(String[] args)
+{
+// instance of Integer type
+Test<Integer> iObj = new Test<Integer>(169593);
+System.out.println(iObj.getObject());
+// instance of String type
+Test<String> sObj
+= new Test<String>("Pratibha");
+System.out.println(sObj.getObject());
+}
+}
 
 
+
+
+
+
+// Aim : Write a Java Program to demonstrate a Generic methods.
 class Test<T> {
     T obj;
     Test(T obj) { this.obj = obj; } // constructor
